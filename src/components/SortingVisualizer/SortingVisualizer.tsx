029024a89729846a3c99bebf4c96a8df1e0f2c8f
Israel Guillermo <israel_guillermo@external.mckinsey.com>
2022-07-26 12:59:52 -0500

    updates colors for primary, secondary and completed. Also updates disabled buttons when sorting.

--- src/components/SortingVisualizer/SortingVisualizer.tsx
+++ src/components/SortingVisualizer/SortingVisualizer.tsx
@@ -2,14 +2,19 @@ import { useEffect, useState } from "react"
 import { getMergeSortAnimations } from "../../utils/SortingAlgorithms/sortingAlgorithms";
 import "./SortingVisualizer.css"
 export function SortingVisualizer() {
-  const AnimationSpeed = 1;
-  const numberOfArrayBars = 310;
+  const AnimationSpeed = 5;
+  const numberOfArrayBars = 300;
   const primaryColor = '#83c5be';
-  const secondaryColor = "red"
+  const secondaryColor = "#d62828"
+  const completedColor = "#489fb5"
+  const [completed, setCompleted] = useState(false)
+  const [isSorting, setIsSorting] = useState(false)
   useEffect(() => {
-    const resetArray = () => {
-      const numArray: number[] = []
 
+    const resetArray = () => {
+
+      const numArray: number[] = []
+      setIsSorting(false)
       for (let i = 0; i < numberOfArrayBars; i++) {
         numArray.push(generateRanInt(5, 730))
       }
@@ -19,6 +24,8 @@ export function SortingVisualizer() {
   }, [])
   const [array, setArray] = useState<number[]>([])
   const resetArray = () => {
+    setIsSorting(false)
+    setCompleted(false)
     const numArray: number[] = []
 
     for (let i = 0; i < numberOfArrayBars; i++) {
@@ -30,9 +37,12 @@ export function SortingVisualizer() {
     return Math.floor(Math.random() * (max - min + 1) + min)
   }
 
-  const handleMergeSort = () => {
+  const handleMergeSort = async () => {
+    await setIsSorting(true)
     const animations = getMergeSortAnimations(array)
+
     for (let i = 0; i < animations.length; i++) {
+
       const arrayBars = document.getElementsByClassName("array-bar") as HTMLCollectionOf<HTMLElement>;
       const isColorChange = i % 3 !== 2;
       if (isColorChange) {
@@ -45,14 +55,18 @@ export function SortingVisualizer() {
           barTwoStyle.backgroundColor = color
 
         }, i * AnimationSpeed)
-      } else {
+      }
+      else {
         setTimeout(() => {
           const [barOneIdx, newHeight] = animations[i]
           const barOneStyle = arrayBars[barOneIdx].style
           barOneStyle.height = `${newHeight}px`
-        }, i * AnimationSpeed)
-      }
 
+          setCompleted(i === animations.length - 1)
+
+        }, i * AnimationSpeed)
+
+      }
 
     }
   }
@@ -60,15 +74,11 @@ export function SortingVisualizer() {
   return (<>
     <div className="array-container">
       {array.map((value, idx) => (
-        <div className="array-bar" key={idx} style={{ height: `${value}px`, backgroundColor: primaryColor }} />
+        <div className="array-bar" key={idx} style={{ height: `${value}px`, backgroundColor: completed ? completedColor : primaryColor }} />
       ))}
 
-
-      {/* <button onClick={ }>Merge Sort</button>
-      <button onClick={ }>Bubble Sort</button>/
-      <button onClick={ }>Selection Sort</button> */}
-      <button onClick={resetArray}>Generate New Array</button>
-      <button onClick={handleMergeSort}>Merge Sort</button>
+      <button disabled={isSorting && !completed} onClick={resetArray}>Generate New Array</button>
+      <button disabled={isSorting} onClick={handleMergeSort}>Merge Sort</button>
 
 
 
